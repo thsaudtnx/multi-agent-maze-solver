7fa1ee48ce1764446cb9e2e862fe87d03dbd9c7a
thsaudtnx <thsaudtnx@gmail.com>
2025-03-12 13:43:12 +0000

    Refactor Tarry's Algorithm with New Agent Class and add sensor variable

--- tarry.py
+++ tarry.py
@@ -1,4 +1,6 @@
 import random
+import math
+from pyamaze import agent, COLOR, maze
 
 """
 Tarry's Algorithm
@@ -12,96 +14,171 @@ the agent should retreat until a cell that meets one of the previous conditions.
 6) When retreating, mark the cells retreated from as “dead end”.
 """
 
-def Tarry(m, num_of_agents):
-    start = (m.rows, m.cols)
-    end = (1, 1)
+# Set Global Variables
+colors = [
+    COLOR.black, 
+    COLOR.blue, 
+    COLOR.cyan, 
+    COLOR.dark, 
+    COLOR.green, 
+    COLOR.light, 
+    COLOR.red, 
+    COLOR.yellow
+]
+        
+class Agent:
 
-    # Create agents
-    dead_end = []
-    paths = {i: [start] for i in range(num_of_agents)}
-    frontier = [(i, start) for i in range(num_of_agents)]
-    explored = {i : [start] for i in range(num_of_agents)}
-    total_explored = [start]
-
-    solver_agent_idx = -1
+    def __init__(self, m, agent_idx, sensor_len = 5):
+        self.agent_idx = agent_idx
+        self.m = m
+        self.agent = agent(m, shape='arrow', footprints=True, color=colors[agent_idx])
+        self.dead_end = []
+        start = (m.rows, m.cols)
+        self.path = [start]
+        self.explored = [start]
+        self.total_explored = [start]
+        self.solver_agent_path = []
+        self.sensor_len = sensor_len
     
-    while frontier:
+    def find_nearby_agents(self, agents):
+        current_x, current_y = self.path[-1]
+        nearby_agents = []
 
-        #print(f"frontier: {frontier}")
+        for agent in agents:
+            
+            agent_x, agent_y = agent.path[-1]
+            distance = math.sqrt((agent_x - current_x) ** 2 + (agent_y - current_y) ** 2)
+
+            if distance <= self.sensor_len:
+                nearby_agents.append(agent)
         
-        cur_agent_idx, cur_location = frontier.pop(0)
+        return nearby_agents
+    
+    def communicate_with_nearby_agents(self, nearby_agents):
+        # update explored cells and dead end from the other agents
+        for agent in nearby_agents:
+            for de in agent.dead_end:
+                if de not in self.dead_end:
+                    self.dead_end.append(de)
+                
+            for te in agent.total_explored:
+                if te not in self.total_explored:
+                    self.total_explored.append(te)
         
-        # this agent solve the maze
-        if cur_location == end:
-            if solver_agent_idx == -1:
-                solver_agent_idx = cur_agent_idx
-            continue
+            if not self.solver_agent_path and agent.path[-1] == (1, 1):
+                self.solver_agent_path = agent.path
+    
+    def move(self):
+        current_location = self.path[-1]
+
+        # if current agent already reach the goal
+        if current_location == (1, 1):
+            return
         
-        # the agent found a solver agent path
-        if solver_agent_idx != -1 and cur_location in paths[solver_agent_idx]:
-            #print(f"solver path: {paths[solver_agent_idx]}")
-            next_location_idx = len(paths[solver_agent_idx]) - list(reversed(paths[solver_agent_idx])).index(cur_location)
-            next_location = paths[solver_agent_idx][next_location_idx]
-            #print(f"next location: {next_location}")
-            frontier.append((cur_agent_idx, next_location))
-            if next_location not in explored[cur_agent_idx]:
-                explored[cur_agent_idx].append(next_location)
-            paths[cur_agent_idx].append(next_location)
-            continue
+        # if current agent is following the solver agent path
+        if current_location in self.solver_agent_path:
+            next_location_idx = len(self.solver_agent_path) - list(reversed(self.solver_agent_path)).index(current_location)
+            next_location = self.solver_agent_path[next_location_idx]
+            if next_location not in self.explored:
+                self.explored.append(next_location)
+            self.path.append(next_location)
+            return
         
-        not_explored_next_location = []
-        possible_next_location = []
+        # distinguish cells which only visited by this agent or the others
+        not_explored_by_this_agent_location = []
+        not_explored_by_all_agent_location = []
 
         for d in 'ESNW':
-            if m.maze_map[cur_location][d] == 1:
+            if self.m.maze_map[current_location][d] == 1:
                 if d == 'E':
-                    next_location = (cur_location[0],cur_location[1]+1)
+                    next_location = (current_location[0],current_location[1]+1)
                 elif d=='W':
-                    next_location=(cur_location[0],cur_location[1]-1)
+                    next_location=(current_location[0],current_location[1]-1)
                 elif d=='N':
-                    next_location=(cur_location[0]-1,cur_location[1])
+                    next_location=(current_location[0]-1,current_location[1])
                 elif d=='S':
-                    next_location=(cur_location[0]+1,cur_location[1])
+                    next_location=(current_location[0]+1,current_location[1])
 
-                # not explored next location
-                if next_location not in total_explored:
-                    possible_next_location.append(next_location)
-                # explored by other agents
-                elif next_location not in explored[cur_agent_idx]:
-                    not_explored_next_location.append(next_location)
-
-        #print(f"possible next location: {possible_next_location}")
-        #print(f"not explored next location: {not_explored_next_location}")
-        #print(f"Current Agent Explored: {explored[cur_agent_idx]}")
-        #print(f"Current Agent paths: {paths[cur_agent_idx]}")
-        #print("----------------------------")
-
+                # all ther agents have not explored the location
+                if next_location not in self.total_explored:
+                    not_explored_by_all_agent_location.append(next_location)
+                # explored by other agents but not by this agent
+                elif next_location not in self.explored:
+                    not_explored_by_this_agent_location.append(next_location)
+        
         # If there are several such cells, the agent should choose one arbitrarily
-        if possible_next_location:
-            random_next_location = random.choice(possible_next_location)
-            frontier.append((cur_agent_idx, random_next_location))
-            explored[cur_agent_idx].append(random_next_location)
-            total_explored.append(random_next_location)
-            paths[cur_agent_idx].append(random_next_location)
+        if not_explored_by_all_agent_location:
+            random_next_location = random.choice(not_explored_by_all_agent_location)
+            self.explored.append(random_next_location)
+            self.total_explored.append(random_next_location)
+            self.path.append(random_next_location)
         
         # If all the locations has been traveled by other agents, the agent should prefer to move to a cell that has not been traveled by it.
-        elif not_explored_next_location:
-            random_next_location = random.choice(not_explored_next_location)
-            frontier.append((cur_agent_idx, random_next_location))
-            explored[cur_agent_idx].append(random_next_location)
-            paths[cur_agent_idx].append(random_next_location)
+        elif not_explored_by_this_agent_location:
+            random_next_location = random.choice(not_explored_by_this_agent_location)
+            self.explored.append(random_next_location)
+            self.path.append(random_next_location)
         
         # If all the possible directions have already been traveled by the agent, 
         # or if the agent has reached a dead-end, 
         # the agent should retreat until a cell that meets one of the previous conditions.
         else:
-            if cur_location not in dead_end:            
-                dead_end.append(cur_location)
-            prev_location_idx = paths[cur_agent_idx].index(cur_location) - 1
-            prev_location = paths[cur_agent_idx][prev_location_idx]
-            frontier.append((cur_agent_idx, prev_location))
-            paths[cur_agent_idx].append(prev_location)
-                
-    #print(f"Dead End: {dead_end}")
-    #print(f"Paths: {paths}")
+            if current_location not in self.dead_end:            
+                self.dead_end.append(current_location)
+            prev_location_idx = self.path.index(current_location) - 1
+            prev_location = self.path[prev_location_idx]
+            self.path.append(prev_location)
+
+def is_solved(agents):
+    for agent in agents:
+        if agent.path[-1] != (1, 1):
+            return False
+    
+    return True
+
+
+def Tarry(m, num_of_agents):
+
+    paths = {}
+    frontier = [Agent(m, agent_idx) for agent_idx in range(num_of_agents)]
+    
+    while frontier:
+
+        agent = frontier.pop(0)
+        print(f"--- Agent_{agent.agent_idx} ---")
+        current_location = agent.path[-1]
+        print(f"Current Location: {agent.path[-1]}")
+        if current_location == (1, 1):
+            frontier.append(agent)
+            continue
+        
+        # agent process
+        nearby_agents = agent.find_nearby_agents(frontier)
+        print(f"Nearby Agents: {[agent.agent_idx for agent in nearby_agents]}")
+        agent.communicate_with_nearby_agents(nearby_agents)
+        agent.move()
+
+        # update the total paths
+        paths[agent] = agent.path
+
+        locations = {f"Agent_{agent.agent_idx}": path[-1] for agent, path in paths.items()}
+
+        print(f"Next Locations: {locations}")
+        print("-----------------------")
+
+        frontier.append(agent)
+
+        # check if all the agents reach the goal
+        if is_solved(frontier):
+            break
+
+    
     return paths
+
+if __name__ == '__main__':
+    m = maze(10, 10)
+    m.CreateMaze(loadMaze="/Users/sonmyungsoo/Downloads/multi-agent-maze-solver/maze/small_100.csv", theme='light')
+    paths = Tarry(m, 2)
+    p = {agent.agent : path for agent, path in paths.items()}
+    m.tracePath(p)
+    m.run()
